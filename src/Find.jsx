92bc891a2fe511d35edafc69683a3058221592bf
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-05-06 12:21:40 -0400

    again

--- src/Find.jsx
+++ src/Find.jsx
@@ -104,8 +104,6 @@ function Find() {
               src="https://discord.com/widget?id=984461709806804992&theme=dark"
               width="350"
               height="500"
-              allowtransparency="true"
-              frameborder="0"
               sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
             ></iframe>
           ) : (
@@ -113,8 +111,6 @@ function Find() {
               src="https://discord.com/widget?id=984461709806804992&theme=dark"
               width="500"
               height="500"
-              allowtransparency="true"
-              frameborder="0"
               sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
             ></iframe>
           )}
