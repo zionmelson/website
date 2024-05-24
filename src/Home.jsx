6198952789b252fbfa8d2143a48c0eadfc958eff
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-05-24 02:46:09 -0400

    names

--- src/Home.jsx
+++ src/Home.jsx
@@ -53,54 +53,21 @@ function Home() {
             <div className="animation">
               {loaded ? (
                 <>
-                  <h2 className="h1" id="animation">
-                    full-stack developer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    back-end developer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    systems engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    software architect
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    regression engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    cloud engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    blockchain engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    devops engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    database engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    data engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    project manager
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    ui/ux designer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    graphic designer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    animation engineer
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    product manager
-                  </h2>
-                  <h2 className="h1" id="animation">
-                    scrum master
-                  </h2>
+                  <h1 className="h1" id="animation">
+                    engineer
+                  </h1>
+                  <h1 className="h1" id="animation">
+                    designer
+                  </h1>
+                  <h1 className="h1" id="animation">
+                    manager
+                  </h1>
+                  <h1 className="h1" id="animation">
+                    recruiter
+                  </h1>
+                  <h1 className="h1" id="animation">
+                    executive
+                  </h1>
                 </>
               ) : (
                 <>
