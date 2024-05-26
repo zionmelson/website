81df87c8f45c9cfe7effc41a670a36d43ee6b03a
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-05-26 04:25:16 -0400

    yes

---
+++ src/Footer.jsx
@@ -0,0 +1,60 @@
+import link from "./images/link.svg";
+import x from "./images/twitter.svg";
+import medium from "./images/medium.svg";
+import spotify from "./images/spotify.svg";
+import discord from "./images/discord.svg";
+
+export default function Footer() {
+  return (
+    <div className="main">
+      <div className="vbox" style={{ gap: "1.5rem" }}>
+        <div className="vbox" style={{ gap: "0.25rem" }}>
+          <p className="p">© 2024 learnmutiny.io. All rights reserved.</p>
+          <a className="p" href="/terms">
+            Terms of Service
+          </a>
+          <a className="p" href="/privacy">
+            Privacy Policy
+          </a>
+        </div>
+        <div className="hbox" style={{ gap: "0.5rem" }}>
+          <a
+            href="https://www.linkedin.com/company/learnmutiny/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="emoji" src={link} alt="link" />
+          </a>
+          <a
+            href="https://www.twitter.com/learnmutiny/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="emoji" src={x} alt="x" />
+          </a>
+          <a
+            href="https://medium.com/@learnmutiny.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="emoji" src={medium} alt="medium" />
+          </a>
+          <a
+            href="https://podcasters.spotify.com/pod/show/learnmutinyio"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="emoji" src={spotify} alt="spot" />
+          </a>
+          <a
+            href="https://discord.com/servers/learnmutiny-io-984461709806804992"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img className="emoji" src={discord} alt="discord" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
