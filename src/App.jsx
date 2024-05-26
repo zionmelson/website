c1e2a2844319bdc48d6208a7e453c783ec1eefcb
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-05-26 04:25:34 -0400

    bye

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { Analytics } from "@vercel/analytics/react";
+import Hotjar from "@hotjar/browser";
+
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./Home";
+import Stats from "./Stats";
+import Privacy from "./Privacy";
+import Terms from "./Terms";
+import Error from "./Error";
+import Footer from "./Footer";
+
+import "./App.css";
+
+const siteId = 3788387;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
