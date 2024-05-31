0b5649f75094cf370420a390dd1a6b05a245f627
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-05-31 17:49:17 -0400

    ds

---
+++ src/Error.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function Error() {
+  return (
+    <div className="main">
+      <h1 className="h1">Let&apos;s get started</h1>
+      <a href="https://discord.gg/WKj3uz6sZZ">
+        <h3 className="h3">
+          <button className="button"> welcome 🚀</button>
+        </h3>
+      </a>
+    </div>
+  );
+}
