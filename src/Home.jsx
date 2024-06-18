9f01661697f549c327d6e1783997cb3313be7812
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-06-18 14:45:04 -0400

    afgin

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable react/prop-types */
 
 import gsap from "gsap";
@@ -85,11 +86,7 @@ function Home() {
           for your startup
         </h1>
         <div className="vbox">
-<<<<<<< HEAD
           <h2 className="h3">Hire our talent all on Discord 🤖</h2>
-=======
-          <h2 className="h3">hire to our tech talent on Discord 🤖</h2>
->>>>>>> bd0c702 (again)
         </div>
         <a href="https://discord.gg/WKj3uz6sZZ">
           <h3 className="h3">
