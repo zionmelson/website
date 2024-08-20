d732375dcf79c57abd8aee87c9da7d7954262e1d
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-08-20 13:22:41 -0400

    again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights";
 import Hotjar from "@hotjar/browser";
 
 import { Route, Routes } from "react-router-dom";
