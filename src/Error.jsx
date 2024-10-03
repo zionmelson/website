c360def339887db06470984584127c8b6d1ea8cc
zionmelson <92884383+zionmelson@users.noreply.github.com>
2024-10-03 09:32:40 -0400

    😶‍🌫️

--- src/Error.jsx
+++ src/Error.jsx
@@ -16,7 +16,7 @@ export default function Error() {
               className="vbox"
               style={{
                 background: "#5964f1",
-                width: "8.5rem",
+                width: "10rem",
                 height: "3.5rem",
                 borderRadius: "0.5rem",
               }}
